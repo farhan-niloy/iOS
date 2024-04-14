07ee9d9d2d0c2d16309d003ad32f612728b185ac
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-04-14 17:05:44 +0600

    first commit

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Farhan Niloy on 4/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
