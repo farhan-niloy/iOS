03deead696b92ab9d728c10787b99b5008dfc716
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-04-14 22:13:09 +0600

    add dicee two

--- Dicee/Dicee-iOS13/ViewController.swift
+++ Dicee/Dicee-iOS13/ViewController.swift
@@ -11,12 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
         diceImageViewOne.alpha = 0.70
+        
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceOne")
+        diceImageViewTwo.alpha = 0.70
         
     }
 
