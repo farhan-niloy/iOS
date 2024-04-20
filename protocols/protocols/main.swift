bcad547a9eb56e9a04cf527ae5a6d01dae064603
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-04-20 14:10:46 +0600

    add clima

--- protocols/protocols/main.swift
+++ protocols/protocols/main.swift
@@ -5,7 +5,69 @@
 //  Created by Farhan Niloy on 4/20/24.
 //
 
-import Foundation
+protocol CanFly {
+    func fly()
+}
 
-print("Hello, World!")
+class Bird {
+    
+    var isFemale = true
+    
+    func layEgg() {
+        if isFemale {
+            print("The bird makes a new bird in a shell.")
+        }
+    }
+}
 
+
+class Eagle: Bird, CanFly{
+    func fly() {
+        print("Eagle flaps its wings and lifts off into the sky.")
+    }
+    
+    
+    func soar() {
+        print("The eagle glides in the air using air currents.")
+    }
+}
+
+class Penguin: Bird {
+    func swim() {
+        print("The penguin paddles through the water")
+    }
+}
+
+struct FlyingMuseum {
+    func flyDemo(flyObject: CanFly) {
+        flyObject.fly()
+    }
+}
+
+class Airplane: CanFly {
+    func fly() {
+        print("The airplane uses its engine to lift off into the air.")
+    }
+}
+
+
+let myEagle = Eagle()
+
+myEagle.fly()
+myEagle.layEgg()
+myEagle.soar()
+
+let myPenguin = Penguin()
+
+myPenguin.swim()
+myPenguin.layEgg()
+
+let museum = FlyingMuseum()
+let airplane = Airplane()
+
+
+museum.flyDemo(flyObject: myEagle)
+museum.flyDemo(flyObject: airplane)
+
+
+airplane.fly()
