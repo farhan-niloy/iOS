5feb378e19f23b3727aa8a52c2f5afafed91e001
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-04-21 16:37:07 +0600

    add calima

--- Clima/Clima/Model/WeatherManager.swift
+++ Clima/Clima/Model/WeatherManager.swift
@@ -19,21 +19,34 @@ struct WeatherManager {
     func performRequest(urlString: String) {
         if  let url = URL(string: urlString) {
             let session = URLSession(configuration: .default)
-            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
+            
+            let task = session.dataTask(with: url) { data, response, error in
+                if(error != nil) {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    self.parseJason(weatherData: safeData)
+                }
+            }
             task.resume()
         }
     }
     
-    func handle(data: Data?, response: URLResponse?, error:Error?) {
-        if(error != nil) {
-            print(error!)
-            return
-        }
-        
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
+    func parseJason(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            let id = decodedData.weather[0].id
             
-            print(dataString)
+            let weather = WeatherModel(conditionId: id, cityName: name, temprature: temp)
+            
+            print(weather.tempratureString)
+        } catch {
+            print(error)
         }
     }
 }
